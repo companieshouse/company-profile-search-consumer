fde5e5d31e1fd7787fb5661852164e9ba5874cc3
kinpang-CH <kpang@companieshouse.gov.uk>
2024-06-04 16:34:34 +0100

    Update gitignore

--- src/main/java/uk/gov/companieshouse/companyprofile/search/Application.java
+++ src/main/java/uk/gov/companieshouse/companyprofile/search/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    public static final String NAMESPACE = "company-profile-delta-consumer";
+    public static final String NAMESPACE = "company-profile-search-consumer";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
