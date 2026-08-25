41f28b9d390b2597815156f117b7fff755fd4656
jbishop2-ch <jbishop2@companieshouse.gov.uk>
2026-08-25 16:41:12 +0100

    Initial draft migration from Spring Boot v3 -> v4

--- Makefile
+++ Makefile
@@ -29,6 +29,10 @@ test-unit:
 test-integration:
 	mvn clean verify -Dskip.unit.tests=true -Dskip.integration.tests=false
 
+.PHONY: docker-image
+docker-image: clean
+	mvn package -Dskip.unit.tests=true -Dskip.integration.tests=true jib:dockerBuild
+
 .PHONY: package
 package:
 ifndef version
@@ -45,12 +49,3 @@ endif
 
 .PHONY: dist
 dist: clean build package
-
-.PHONY: sonar
-sonar:
-	mvn sonar:sonar
-
-.PHONY: sonar-pr-analysis
-sonar-pr-analysis:
-	mvn verify -Dskip.unit.tests=true -Dskip.integration.tests=true
-	#mvn sonar:sonar -P sonar-pr-analysis #temporary until sonar available for Java 21
--- src/main/java/uk/gov/companieshouse/companyprofile/search/consumer/SearchConsumer.java
+++ src/main/java/uk/gov/companieshouse/companyprofile/search/consumer/SearchConsumer.java
@@ -1,13 +1,14 @@
 package uk.gov.companieshouse.companyprofile.search.consumer;
 
 import consumer.exception.NonRetryableErrorException;
+import org.jspecify.annotations.NonNull;
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.kafka.retrytopic.SameIntervalTopicReuseStrategy;
 import org.springframework.messaging.Message;
-import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.companyprofile.search.logging.DataMapHolder;
 import uk.gov.companieshouse.companyprofile.search.processor.SearchProcessor;
@@ -18,14 +19,13 @@ import uk.gov.companieshouse.stream.ResourceChangedData;
 public class SearchConsumer {
 
     private final Logger logger;
-    public final KafkaTemplate<String, Object> kafkaTemplate;
+    public final KafkaTemplate<@NonNull String, @NonNull Object> kafkaTemplate;
     private final SearchProcessor searchProcessor;
 
     /**
      * Consumes messages from stream-company-profile.
      */
-    public SearchConsumer(Logger logger, KafkaTemplate<String, Object> kafkaTemplate,
-            SearchProcessor searchProcessor) {
+    public SearchConsumer(Logger logger, KafkaTemplate<@NonNull String, @NonNull Object> kafkaTemplate,SearchProcessor searchProcessor) {
         this.logger = logger;
         this.kafkaTemplate = kafkaTemplate;
         this.searchProcessor = searchProcessor;
@@ -36,7 +36,7 @@ public class SearchConsumer {
      */
     @RetryableTopic(attempts = "${company-profile.search.retry-attempts}",
             sameIntervalTopicReuseStrategy = SameIntervalTopicReuseStrategy.SINGLE_TOPIC,
-            backoff = @Backoff(delayExpression = "${company-profile.search.backoff-delay}"),
+            backOff = @BackOff(delayString = "${company-profile.search.backoff-delay}"),
             retryTopicSuffix = "-${company-profile.search.group-id}-retry",
             dltTopicSuffix = "-${company-profile.search.group-id}-error",
             dltStrategy = DltStrategy.FAIL_ON_ERROR,
@@ -46,7 +46,7 @@ public class SearchConsumer {
             topics = "${company-profile.search.topic}",
             groupId = "${company-profile.search.group-id}",
             containerFactory = "listenerContainerFactory")
-    public void receive(Message<ResourceChangedData> resourceChangedMessage) {
+    public void receive(Message<@NonNull ResourceChangedData> resourceChangedMessage) {
         String eventType = resourceChangedMessage.getPayload().getEvent().getType();
         if (eventType.equals("changed")) {
             searchProcessor.processChangedMessage(resourceChangedMessage);
